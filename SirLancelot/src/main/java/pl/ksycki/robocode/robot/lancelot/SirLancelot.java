5f8d9a4dc737c5f8ef8b7e864e78cb0f610f847d
bksycki <blazej@ksycki.pl>
2012-07-15 17:11:22 +0200

    Empty robot project

---
+++ SirLancelot/src/main/java/pl/ksycki/robocode/robot/lancelot/SirLancelot.java
@@ -0,0 +1,12 @@
+package pl.ksycki.robocode.robot.lancelot;
+
+import robocode.AdvancedRobot;
+
+public class SirLancelot extends AdvancedRobot {
+	
+	@Override
+	public void run() {
+		super.run();
+	}
+
+}
